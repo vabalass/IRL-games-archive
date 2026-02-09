3549102da2ba74b409f05e5833196260a9297f3d
vabalass <vabalas.balys@gmail.com>
2026-02-09 09:43:56 +0200

    removed unused code and comments

--- games_project/games/selectors.py
+++ games_project/games/selectors.py
@@ -1,15 +1,17 @@
-from django.db.models import Avg, Q, Count
+from django.db.models import Avg
+from django.db.models import Count
 
 from .models import GameWithStats
+
 
 def games_that_have_comments_with_rating():
     return GameWithStats.objects.filter(
         comments__rating__isnull=False,
     ).distinct()
 
+
 def games_anotated_with_stats():
-    # paimti visus games, kiekvienam surasti komentarus, kurie turi reitingą ir neturi tėvų, anotuoti avg_rating stulpelį 
     return games_that_have_comments_with_rating().annotate(
-        avg_rating=Avg('comments__rating'),
-        comments_count=Count("comments", distinct=True)
+        avg_rating=Avg("comments__rating"),
+        comments_count=Count("comments", distinct=True),
     )
