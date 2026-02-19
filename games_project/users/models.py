60ef94ea3429c4e671b6d7291a2be89a6738963d
Balys Žalneravičius <91499666+vabalass@users.noreply.github.com>
2026-02-19 09:20:27 +0200

    Track user IP (#9)
    * created title decorator
    
    * created custom Game stats fields
    
    * fixed naming and formating
    
    * fixed property names in admin
    
    * actions are not added to all admins and created NoDeleteMixin
    
    * removed delete actions and created
    
    added admin action that resets rating
    
    actions are not added to all admins and created NoDeleteMixin
    
    * Implemented middleware that saves user ip address once a day
    
    Implemented middleware that saves user ip address once a day
    
    Implemented middleware that saves user ip address once a day
    
    * rebase problems
    
    * created Ip address model
    
    * added user_ip table
    
    * squashed migrations
    
    * fixed user ips update method, removed unused fields
    
    * fixed migrations

--- games_project/users/models.py
+++ games_project/users/models.py
@@ -1,7 +1,35 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class UserIp(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="ips",
+    )
+    ip_address = models.GenericIPAddressField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+        verbose_name_plural = "User IPs"
+
+    def __str__(self):
+        return f"{self.user.username}: {self.ip_address}"
+
+    @classmethod
+    def save_ip_if_new(cls, user, ip_address):
+        today = timezone.now().date()
+
+        if cls.objects.filter(user=user, created__date=today).exists():
+            return None
+
+        return cls.objects.create(user=user, ip_address=ip_address)
 
 
 class User(AbstractUser):
@@ -12,7 +40,7 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
